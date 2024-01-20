7faaf72d227512668e327b94a088bcfc5f0b1afb
Matt Michie <mmichie@gmail.com>
2024-01-20 15:40:30 -0800

    Refactor Handler Config and Main Logic
    - Streamlined configuration parsing in `config.rs`, including renaming fields for clarity and
      simplifying the struct for handler options.
    - Updated `main.rs` to use the new configuration structure, added essential logging, and handled
      unimplemented handler types with warnings.

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,5 @@
-use log::{error, info};
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::{error::Error, fs};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -9,14 +8,14 @@ pub struct Config {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HandlerConfig {
-    pub handler: String,
+    #[serde(rename = "type")]
     pub event_type: EventType,
-    pub options: HandlerOptions,
-    pub action: ActionConfig,
+    pub name: String,     // Renamed from 'handler' to 'name'
+    pub options: Options, // Changed to a struct to match the YAML format
+    pub shell: String,    // Directly included as a field
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
 pub enum EventType {
     SQS,
     WebPolling,
@@ -27,61 +26,19 @@ pub enum EventType {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct HandlerOptions {
-    #[serde(flatten)]
-    pub specific: SpecificOptions,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-pub enum SpecificOptions {
-    SQS {
-        queue_url: String,
-        poll_interval: u64,
-    },
-    WebPolling {
-        url: String,
-        poll_interval: u64,
-    },
-    Cron {
-        schedule: String,
-    },
-    Webhook {
-        port: u16,
-        path: String,
-    },
-    Filesystem {
-        path: String,
-        event: String,
-    },
-    Database {
-        db_connection: String,
-        query: String,
-        interval: u64,
-    },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ActionConfig {
-    pub shell: String,
+pub struct Options {
+    #[serde(rename = "type")]
+    pub options_type: String, // To handle the nested 'type' field in options
+    pub queue_url: Option<String>,
+    pub poll_interval: Option<u64>,
+    pub port: Option<u16>,
+    pub path: Option<String>,
+    pub aws_region: Option<String>,
 }
 
 // Function to load and parse the YAML configuration file
-pub fn load_config(file_path: &str) -> Result<Config, Box<dyn std::error::Error>> {
-    match fs::read_to_string(file_path) {
-        Ok(config_str) => {
-            info!("Config file read successfully.");
-            match serde_yaml::from_str(&config_str) {
-                Ok(config) => Ok(config),
-                Err(e) => {
-                    error!("Failed to parse config file: {}", e);
-                    Err(Box::new(e))
-                }
-            }
-        }
-        Err(e) => {
-            error!("Failed to read config file: {}", e);
-            Err(Box::new(e))
-        }
-    }
+pub fn load_config(file_path: &str) -> Result<Config, Box<dyn Error>> {
+    let config_str = fs::read_to_string(file_path)?;
+    let config: Config = serde_yaml::from_str(&config_str)?;
+    Ok(config)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod config;
 use clap::{Arg, Command};
-use log::{error, info};
+use log::{error, info, warn};
 use tokio;
 
 mod handlers {
@@ -8,11 +8,12 @@ mod handlers {
     pub mod webhook_handler;
 }
 
-use crate::config::{load_config, EventType, SpecificOptions};
+use crate::config::{load_config, EventType};
 use crate::handlers::{sqs_handler, webhook_handler};
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let matches = Command::new("Arcnar")
         .version("0.0.1")
         .author("Matt Michie")
@@ -40,39 +41,38 @@ async fn main() {
     for handler_config in config.handlers {
         match handler_config.event_type {
             EventType::SQS => {
-                if let SpecificOptions::SQS {
-                    queue_url,
-                    poll_interval,
-                } = handler_config.options.specific
-                {
-                    // Clone and move the queue_url into the async block
-                    let sqs_future = tokio::spawn(async move {
-                        sqs_handler::sqs_poller(queue_url, poll_interval).await
-                    });
-                    handler_futures.push(sqs_future);
+                if let Some(queue_url) = handler_config.options.queue_url {
+                    if let Some(poll_interval) = handler_config.options.poll_interval {
+                        info!("Initializing SQS handler for queue: {}", queue_url);
+                        let sqs_future = tokio::spawn(async move {
+                            sqs_handler::sqs_poller(queue_url, poll_interval).await
+                        });
+                        handler_futures.push(sqs_future);
+                    }
                 }
             }
             EventType::Webhook => {
-                if let SpecificOptions::Webhook { port, path } = handler_config.options.specific {
-                    // Clone and move the path into the async block
-                    let webhook_future =
-                        tokio::spawn(
-                            async move { webhook_handler::webhook_listener(port, path).await },
-                        );
-                    handler_futures.push(webhook_future);
+                if let Some(port) = handler_config.options.port {
+                    if let Some(path) = handler_config.options.path {
+                        info!("Initializing Webhook handler on port {} with path {}", port, path);
+                        let webhook_future = tokio::spawn(async move {
+                            webhook_handler::webhook_listener(port, path).await
+                        });
+                        handler_futures.push(webhook_future);
+                    }
                 }
             }
             EventType::WebPolling => {
-                log::warn!("WebPolling is not yet implemented");
+                warn!("WebPolling is not yet implemented");
             }
             EventType::Cron => {
-                log::warn!("Cron is not yet implemented");
+                warn!("Cron is not yet implemented");
             }
             EventType::Filesystem => {
-                log::warn!("Filesystem is not yet implemented");
+                warn!("Filesystem is not yet implemented");
             }
             EventType::Database => {
-                log::warn!("Database is not yet implemented");
+                warn!("Database is not yet implemented");
             }
         }
     }
